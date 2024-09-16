f995456d7643804efcbd3d043b34022afc7839d7
sbx_user1051 <sbx_user1051@169.254.39.113>
2024-09-16 20:17:27 +0000

    Mon Sep 16 20:17:27 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,24 +8,29 @@ PRICES: Dict[str, int] = {
     'E': 40
 }
 
-OFFERS: Dict[str, int] = {
+OFFERS: Dict[str, List[Tuple[int, int]]] = {
     'A': [(5,200), (3, 130)],
     'B': [(2, 45)]
 }
 
-DEALS: Dict[str, int] = {
-    'A': 50,
-    'B': 30,
-    'C': 20,
-    'D': 15,
-    'E': 40
+DEALS: Dict[str, Tuple[int, str, int]] = {
+    'E': (2, 'B', 1)
 }
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
 
+    if not isinstance(skus, str):
+        return -1
 
+    def count_items(skus: str) -> Optional[Dict[str, int]]:
+        item_counts: Dict[str, int] = {}
+
+        for sku in skus:
+            if sku not in PRICES:
+                return None
+            item_counts[sku] = item_counts.get(sku, 0) + 1
 
     # function to calculate the cost of a given item and quantity 
     def calculate_item_cost(sku: str, qty: int) -> Optional[int]:
@@ -44,14 +49,7 @@ def checkout(skus: str) -> int:
         else:
             return qty * item_price
         
-    # count the items 
-    item_counts: Dict[str, int] = {}
 
-    if not isinstance(skus, str):
-        return -1
-
-    for sku in skus:
-        item_counts[sku] = item_counts.get(sku, 0) + 1
     
     total = 0
 
@@ -69,5 +67,6 @@ def checkout(skus: str) -> int:
 
 
     
+
 
 
