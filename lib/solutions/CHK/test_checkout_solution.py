57298879b93a2d992e254671c928e7002fd20bcb
sbx_user1051 <sbx_user1051@169.254.39.113>
2024-09-16 21:05:08 +0000

    Mon Sep 16 21:05:08 UTC 2024

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -5,6 +5,7 @@ def test_checkout():
     assert checkout("A") == 50
     assert checkout(76) == -1
     assert checkout("ATYEBC") == -1
+    assert checkout("£$AC@") == -1
     assert checkout("AB") == 80
     assert checkout("BBBB") == 90    
     assert checkout("AAAAAA") == 250
@@ -12,3 +13,9 @@ def test_checkout():
     assert checkout("ABCDE") == 155
     assert checkout("EEBBB") == 125
     assert checkout("AAAAAEEBAAABB") == 455 
+    assert checkout("FF") == 20
+    assert checkout("FFF") == 20
+    assert checkout("FFFF") == 30
+    assert checkout("FFFFFEE") == 90
+
+
