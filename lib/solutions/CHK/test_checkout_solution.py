6f5f4c8b7a1b2ed877c659528a5f5730c0563ccb
sbx_user1051 <sbx_user1051@169.254.39.113>
2024-09-16 21:29:03 +0000

    Mon Sep 16 21:29:03 UTC 2024

--- lib/solutions/CHK/test_checkout_solution.py
+++ lib/solutions/CHK/test_checkout_solution.py
@@ -1,17 +1,21 @@
 from .checkout_solution import checkout
 
 def test_checkout():
-    assert checkout("") == 0
-    assert checkout("A") == 50
+    
     assert checkout(76) == -1
     assert checkout("£$AC@") == -1
+
+    assert checkout("") == 0
+    assert checkout("A") == 50
     assert checkout("AB") == 80
     assert checkout("BBBB") == 90    
     assert checkout("AAAAAA") == 250
-    assert checkout("AAAABBCD") == 260   
+    assert checkout("AAAABBCD") == 260  
+
     assert checkout("ABCDE") == 155
     assert checkout("EEBBB") == 125
     assert checkout("AAAAAEEBAAABB") == 455 
+
     assert checkout("FF") == 20
     assert checkout("FFF") == 20
     assert checkout("FFFF") == 30
@@ -22,18 +26,19 @@ def test_checkout():
 
     assert checkout("KK") == 150
 
-    # assert checkout("NNNNM") == 175
+    assert checkout("NNNNM") == 160
 
     assert checkout("PPPPP") == 200
 
-    assert checkout("RRRQ") == 180
+    assert checkout("RRRQ") == 150
     assert checkout("RRRRRRQQQ") == 330
 
     assert checkout("UUUU") == 120
 
     assert checkout("VVVV") == 180
 
-    assert checkout("STXYZ") == 98
+    assert checkout("STXYZ") == 200
+
 
 
 
