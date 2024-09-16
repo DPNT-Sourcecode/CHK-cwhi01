ead74bdde79a0ac951c1baa772977457f762e0ef
sbx_user1051 <sbx_user1051@169.254.39.113>
2024-09-16 19:32:13 +0000

    Mon Sep 16 19:32:13 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,6 +6,7 @@ def checkout(skus: str) -> int:
     # first value in the value tuple is price of the item, second is the special offer if there is one (qty and the offer price)
     # sku -> (price, special_offer)
     # special offer is a tuple (qty, special_price) or None
+    # Note: can be further refactored with the use of nested dicts for better readability
     price_table = {
         'A': (50, (3, 130)),
         'B': (30, (2, 45)),
@@ -23,10 +24,13 @@ def checkout(skus: str) -> int:
         if special_offer:
             offer_qty = special_offer[0]
             offer_price = special_offer[1]
-            pass
+            count = qty // offer_qty
+            remaining = qty % offer_qty
+            return (count * offer_price) + (remaining * item_price)
         else:
             return qty * item_price
 
 
     
+
 
