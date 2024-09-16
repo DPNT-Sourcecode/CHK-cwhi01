15ecac36399f990458c2e9840bf2420fab6ad1b4
sbx_user1051 <sbx_user1051@169.254.39.113>
2024-09-16 19:35:13 +0000

    Mon Sep 16 19:35:13 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,20 +1,22 @@
-
+from typing import Dict, Tuple, Optional
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
+
     # first value in the value tuple is price of the item, second is the special offer if there is one (qty and the offer price)
     # sku -> (price, special_offer)
     # special offer is a tuple (qty, special_price) or None
     # Note: can be further refactored with the use of nested dicts for better readability
-    price_table = {
+    price_table: Dict[str, Tuple[int, Optional[Tuple[int, int]]]] = {
         'A': (50, (3, 130)),
         'B': (30, (2, 45)),
         'C': (20, None),
         'D': (15, None)
     }
 
-    def calculate_item_cost(sku, qty):
+    # function to calculate the cost of a given item and quantity 
+    def calculate_item_cost(sku: str, qty: int) -> Optional[int]:
         if sku not in price_table:
             return None
         
@@ -29,8 +31,17 @@ def checkout(skus: str) -> int:
             return (count * offer_price) + (remaining * item_price)
         else:
             return qty * item_price
+        
+    # count the items 
+    item_counts: Dict[str, int] = {}
+
+        for sku in skus:
+
+    
+
 
 
     
+
 
 
