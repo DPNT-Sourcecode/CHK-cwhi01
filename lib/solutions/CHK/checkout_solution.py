6bed7f4c86a6f9a646eddbbe4c0921483843b808
sbx_user1051 <sbx_user1051@169.254.39.113>
2024-09-16 19:20:13 +0000

    Mon Sep 16 19:20:13 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,12 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+    # first value in the value tuple is price of the item, second is the special offer if there is one (qty and the offer price)
+    price_table = {
+        'A': (50, (3, 130)),
+        'B': (30, (2, 45)),
+        'C': (20, None),
+        'D': (15, None)
+    }
+
+
